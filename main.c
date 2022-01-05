d7e118d6cf0bce9a3733cec5da7fd04c8701c86e
Vialomur <dmytruk.m@donnu.edu.ua>
2022-01-05 18:53:31 +0300

    cos poszlo nie tak

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 int dodaj(int num1, int num2);
 int odjac(int num1, int num2);
 int main() {
-    printf("Hello, World!\n");
-    printf("Hello D!");
-    printf("%d", dodaj(127,-843));
+
     printf("%d", odjac(127,-843));
     return 0;
 }
