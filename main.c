e097c77770db8b7b90bfb4f0644639e806e441a0
Vialomur <dmytruk.m@donnu.edu.ua>
2022-01-05 18:50:17 +0300

    jdjac juz tutaj

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 int dodaj(int num1, int num2);
+int odjac(int num1, int num2);
 int main() {
     printf("Hello, World!\n");
     printf("Hello D!");
     printf("%d", dodaj(127,-843));
+    printf("%d", odjac(127,-843));
     return 0;
 }
 int dodaj(int num1, int num2)
 {
     return num1+num2;
+}
+int odjac(int num1, int num2)
+{
+    return num1-num2;
 }
