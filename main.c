3c80cf323092b778819fbcd2d2212eae9b670f8a
Vialomur <dmytruk.m@donnu.edu.ua>
2022-01-05 18:45:21 +0300

    dodałem dodaj

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
+int dodaj(int num1, int num2);
 int main() {
     printf("Hello, World!\n");
+    printf("Hello D!");
+    printf("%d", dodaj(127,-843));
     return 0;
+}
+int dodaj(int num1, int num2)
+{
+    return num1+num2;
 }
